85fdbd6f7170b5d7e18daa3a83c65827f054d9bd
Jon Ruano <ruanoj@users.noreply.github.com>
2015-08-20 22:24:26 +0100

    add restrospective usage view page of a device/interface for Watch4net

---
+++ random-bits/w4n-infourl.php
@@ -0,0 +1,59 @@
+<?php
+// infourl is a quick hack to provide accumulated data of the last
+// 4, 24, 48 hours, last week, month, 3 months and year of a given
+// device and part
+//
+// It invokes w4n-overlibgraph.php as img source
+// Based on EMC M&R/Watch4net Frontend Web Service Developer Tutorial sample code
+// Adapted by @jonruano
+// https://github.com/ruanoj/network-weathermap
+// Released under the GNU Public License
+
+
+// retrieve filter param
+if(isset($_GET['filter'])) {
+  $filter = $_GET['filter'];
+} else {
+  $filter = '';
+}
+// retrieve width param
+if(isset($_GET['width'])) {
+  $width = $_GET['width'];
+} else {
+  $width = 500;
+}
+// retrieve width param
+if(isset($_GET['height'])) {
+  $height = $_GET['height'];
+} else {
+  $height = 300;
+}
+// retrieve scale param
+if(isset($_GET['scale'])) {
+  $scale = $_GET['scale'];
+} else {
+  $scale = 1.0;
+}
+
+ $_baseURL = "w4n-overlibgraph.php?filter=" . urlencode($filter) . "&width=$width&height=$height&scale=" . urlencode($scale);
+?>
+<html><head><title><?= $filter; ?> - Usage Statistics</title>
+<body bgcolor="#ffffff">
+<h3><?= $filter; ?></h3>
+<p>Last four hours:<br/>
+<img border="0" src="<?= $_baseURL; ?>&start=-14400" /></p>
+<p>Last 24 hours:<br/>
+<img border="0" src="<?= $_baseURL; ?>&start=-86400" /></p>
+<p>Last 48 hours:<br/>
+<img border="0" src="<?= $_baseURL; ?>&start=-172800" /></p>
+<p>Last week:<br/>
+<img border="0" src="<?= $_baseURL; ?>&start=-604800" /></p>
+<!-- The following take longer or time out. Perhaps w4n-overlibgraph should call a different function -->
+<p>Last month:<br/>
+<img border="0" src="<?= $_baseURL; ?>&start=-2592000" /></p>
+<p>Last 3 months:<br/>
+<img border="0" src="<?= $_baseURL; ?>&start=-7776000" /></p>
+<p>Last year:<br/>
+<img border="0" src="<?= $_baseURL; ?>&start=-30758400" /></p>
+<hr />
+</body></html>
